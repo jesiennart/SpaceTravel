809f238bbf94f1d079a2da55954635bb8a62c351
“artur” <molart1974@gmail.com>
2023-12-12 13:48:41 +0100

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx'
+
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(<App />);
