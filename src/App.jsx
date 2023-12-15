46c327e8a998a558b97edd4fea98fdc909a710c4
“artur” <molart1974@gmail.com>
2023-12-15 12:01:30 +0100

    add readme file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Home from "./Components/Homepage/Home";
 import Nav from "./Components/Homepage/Nav";
+import Main from "./Components/Homepage/Main";
+import Circle from "./Components/Homepage/Circle";
 import Destinations from "./Components/Destinations/Destinations";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -8,6 +10,8 @@ function App() {
   return (
     <Router>
       <Nav />
+      <Main />
+      <Circle />
       <Routes>
         <Route exact path="/home" element={<Home />} />
         <Route path="/destinations" element={<Destinations />} />
