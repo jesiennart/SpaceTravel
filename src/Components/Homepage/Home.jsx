73e33fc884d622b34e0a6a8b51876b885e4b5d40
“artur” <molart1974@gmail.com>
2024-01-02 12:44:57 +0100

    change in home.jsx

--- src/Components/Homepage/Home.jsx
+++ src/Components/Homepage/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from '../Navbar/Nav'
 import Main from './Main';
 import Circle from './Circle';
-import '../Homepage/home.css';
+import '../Homepage/Home.css';
 
 function Home() {
     return (
